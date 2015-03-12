0a1f250c73d81f3f8f3cca4c32fe4d07787d71b1
IonutCorlau <ionut.corlau@gmail.com>
2015-03-12 22:53:01 +0200

    js added

---
+++ js/validateJQueryPlugin.js
@@ -0,0 +1,111 @@
+$(document).ready(function(){
+    $("#loginForm").validate({
+	rules: {
+	    usernameLogin: {
+	    	required: true,
+	    	minlength: 6,
+	    	maxlength: 25
+	    },
+		passwordLogin: {
+			required: true,
+			minlength: 8,
+			maxlength: 25
+		}
+			},
+	messages: {
+		usernameLogin: {
+			required: "Please specify your Username",
+			minlength: "Username must be least 6 characters long"
+		},
+		passwordLogin: {
+			required: "Please specify your Password",
+			minlength: "Password must be least 8 characters long "
+		}
+			 }
+});
+
+
+	 $("#forgetPasswordForm").validate({
+			rules: {
+			    usernamePasswordForget: {
+			    	required: true,
+			    	minlength: 4,
+			    	maxlength: 35
+			    }
+					},
+			messages: {
+				usernamePasswordForget: {
+					required: "Please enter your Username or Password",
+					minlength: "Please enter valid data"
+				}
+					 }
+		});
+
+	 $("#register").validate({
+			rules: {
+			    firstName: {
+			    	required: true,
+			    	minlength: 3,
+			    	maxlength: 25
+			    },
+			    lastName: {
+			    	required: true,
+			    	minlength: 3,
+			    	maxlength: 25
+			    },
+			    username: {
+			    	required: true,
+			    	minlength: 6,
+			    	maxlength: 25
+			    },
+			    email: {
+			    	required: true,
+			    	email: true,
+			    	maxlength: 35
+			    },
+			    password: {
+			    	required: true,
+			    	minlength: 8,
+			    	maxlength: 25
+			    },
+			    confirmPassword: {
+			    	required: true,
+			    	minlength: 8,
+			        maxlength: 25,
+			    	equalTo: "#password"
+			    }
+
+					},
+			messages: {
+				firstName: {
+					required: "Please specify your First Name",
+					minlength: "First Name must be least 3 characters long"
+				},
+				lastName: {
+					required: "Please specify your Last Name",
+					minlength: "Last Name must be least 3 characters long"
+				},
+				username: {
+					required: "Please specify your Username",
+					minlength: "Username must be least 6 characters long"
+				},
+				email: {
+					required: "Please specify your Email",
+					email: "Please enter a valid email address"
+				},
+				password: {
+					required: "Please specify your Password",
+					minlength: "Password must be least 8 characters long "
+				},
+				confirmPassword: {
+					required: "Please confirm your Password",
+					minlength: "Password must be least 8 characters long",
+					equalTo: "Please enter the same password as above"
+
+				}
+
+
+
+					 }
+		});
+});
